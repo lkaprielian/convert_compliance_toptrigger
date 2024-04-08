d5aceba1e50197c507374dbdd700ec0398b04abf
lkaprielian <lisakaprielian.lk@gmail.com>
2024-04-08 11:38:06 +0200

    add problem

--- actions/CControllerBGAvailReport.php
+++ actions/CControllerBGAvailReport.php
@@ -14,216 +14,80 @@ use CWebUser;
 abstract class CControllerBGAvailReport extends CController {
 
 	// Filter idx prefix.
-	const FILTER_IDX = 'web.avail_report.filter';
-	// const FILTER_IDX = 'web.toptriggers.filter';
-	// const FILTER_IDX = 'web.monitoring.host';
-	// const FILTER_IDX = 'web.monitoring.problem';
-
-
+	const FILTER_IDX = 'web.monitoring.problem';
 
 	// Filter fields default values.
 	const FILTER_FIELDS_DEFAULT = [
-		'name' => '',
-		'mode' => AVAILABILITY_REPORT_BY_TEMPLATE,
-		'tpl_groupids' => [],
-		'templateids' => [],
-		'tpl_triggerids' => [],
-		'triggerids' => [],
-		'hostgroupids' => [],
+		'show' => TRIGGERS_OPTION_RECENT_PROBLEM,
+		'groupids' => [],
 		'hostids' => [],
-		'only_with_problems' => 1,
-        'page' => null,
+		'triggerids' => [],
+		'name' => '',
+		'severities' => [],
+		'age_state' => 0,
+		'age' => 14,
+		'inventory' => [],
+		'evaltype' => TAG_EVAL_TYPE_AND_OR,
+		'tags' => [],
+		'show_tags' => SHOW_TAGS_3,
+		'show_symptoms' => 0,
+		'show_suppressed' => 0,
+		'unacknowledged' => 0,
+		'compact_view' => 0,
+		'show_timeline' => ZBX_TIMELINE_ON,
+		'details' => 0,
+		'highlight_row' => 0,
+		'show_opdata' => OPERATIONAL_DATA_SHOW_NONE,
+		'tag_name_format' => TAG_NAME_FULL,
+		'tag_priority' => '',
+		'page' => null,
+		'sort' => 'clock',
+		'sortorder' => ZBX_SORT_DOWN,
 		'from' => '',
-		'to' => '',
-		'sort' => 'name',
-		'sortorder' => ZBX_SORT_DOWN
+		'to' => ''
 	];
 
-	protected function getData(array $filter): array {
+	/**
+	 * Get count of resulting rows for specified filter.
+	 *
+	 * @param array $filter   Filter fields values.
+	 *
+	 * @return int
+	 */
+	protected function getCount(array $filter): int {
+		$range_time_parser = new CRangeTimeParser();
+		$range_time_parser->parse($filter['from']);
+		$filter['from'] = $range_time_parser->getDateTime(true)->getTimestamp();
+		$range_time_parser->parse($filter['to']);
+		$filter['to'] = $range_time_parser->getDateTime(false)->getTimestamp();
 
-		$host_group_ids = sizeof($filter['hostgroupids']) > 0 ? $this->getChildGroups($filter['hostgroupids']) : null;
+		$data = CScreenProblem::getData($filter, CSettingsHelper::get(CSettingsHelper::SEARCH_LIMIT));
 
-		$generating_csv_flag = 1;
-		if (!array_key_exists('action_from_url', $filter) ||
-			$filter['action_from_url'] != 'availreport.view.csv') {
-			// Generating for UI
-			// $limit = CSettingsHelper::get(CSettingsHelper::SEARCH_LIMIT) + 1;
-			$limit = CSettingsHelper::get(CSettingsHelper::SEARCH_LIMIT) + 130000;
-			$generating_csv_flag = 0;
-		} else {
-			// Generating for CSV report
-			$limit = 5001;
-		}
-		// All CONFIGURED triggers that fall under selected filter
-		$num_of_triggers = API::Trigger()->get([
-			'output' => ['triggerid', 'description', 'expression', 'value'],
-			'monitored' => true,
-			'triggerids' => sizeof($filter['triggerids']) > 0 ? $filter['triggerids'] : null,
-			'groupids' => $host_group_ids,
-			'hostids ' => sizeof($filter['hostids']) > 0 ? $filter['hostids'] : null,
-			'filter' => [
-				'templateid' => sizeof($filter['tpl_triggerids']) > 0 ? $filter['tpl_triggerids'] : null
-			],
-			'countOutput' => true
-		]);
-		$warning = null;
-		if ($num_of_triggers > $limit) {
-			$warning = 'WARNING: ' . $num_of_triggers . ' triggers found which is more than reasonable limit ' . $limit . ', results below might be not totally accurate. Please add or review current filter conditions.';
-		}
+		return count($data['problems']);
+	}
 
-		// Get timestamps from and to
-		if ($filter['from'] != '' && $filter['to'] != '') {
-			$range_time_parser = new CRangeTimeParser();
-			$range_time_parser->parse($filter['from']);
-			$filter['from_ts'] = $range_time_parser->getDateTime(true)->getTimestamp();
-			$range_time_parser->parse($filter['to']);
-			$filter['to_ts'] = $range_time_parser->getDateTime(false)->getTimestamp();
-		} else {
-			$filter['from_ts'] = null;
-			$filter['to_ts'] = null;
-		}
-
-
-		// get 100 triggerids with max event count
-		$triggersEventCount = [];
-		$sql = 'SELECT e.objectid,count(distinct e.eventid) AS cnt_event'.
-				' FROM triggers t,events e'.
-				' WHERE t.triggerid=e.objectid'.
-					' AND e.source='.EVENT_SOURCE_TRIGGERS.
-					' AND e.value='.TRIGGER_VALUE_TRUE.
-					' AND e.clock>='.zbx_dbstr($filter['from_ts']).
-					' AND e.clock<='.zbx_dbstr($filter['to_ts']);
-		
-		$sql .= ' AND '.dbConditionInt('t.flags', [ZBX_FLAG_DISCOVERY_NORMAL, ZBX_FLAG_DISCOVERY_CREATED]).
-				' GROUP BY e.objectid'.
-				' ORDER BY cnt_event DESC';
-		$result = DBselect($sql, 100);
-		while ($row = DBfetch($result)) {
-			$triggersEventCount[$row['objectid']] = $row['cnt_event'];
-		}
-
-		// retrieve only triggerid which is in the filter and exist in triggereventcount(filter + sql result) use it in trigger api call
-		$matched_triggerids = array_intersect(array_keys($triggersEventCount), $filter['triggerids']);
-
-
-		$triggers = API::Trigger()->get([
-			'output' => ['triggerid', 'description', 'expression', 'value', 'priority', 'lastchange'],
-			'selectHosts' => ['hostid', 'status', 'name'],
-			'triggerids' => sizeof($filter['triggerids']) > 0 ? $matched_triggerids : array_keys($triggersEventCount), //added to get top 100 
-			'selectTags' => 'extend',
-			'selectFunctions' => 'extend',
-			'expandDescription' => true,
-			'monitored' => true,
-			'groupids' => $host_group_ids,
-			'preservekeys' => true,
-			'hostids' => sizeof($filter['hostids']) > 0 ? $filter['hostids'] : null,
-			'filter' => [
-				'templateid' => sizeof($filter['tpl_triggerids']) > 0 ? $filter['tpl_triggerids'] : null,
-			],
-            'limit' => $limit
-        ]);
-
-		## added for cnt and hostid
-		foreach ($triggers as $triggerId => $trigger) {
-			$triggers[$triggerId]['cnt_event'] = $triggersEventCount[$triggerId];
-		}
-
-		// Now just prepare needed data. Modified to take 2 ways of sorts
-		$filter['sortorder'] == 'ASC' ? CArrayHelper::sort($triggers, [
-			['field' => 'cnt_event', 'order' => ZBX_SORT_UP],
-			'host', 'description', 'priority'
-		]): CArrayHelper::sort($triggers, [
-			['field' => 'cnt_event', 'order' => ZBX_SORT_DOWN],
-			'host', 'description', 'priority'
-		]);
-
-		$view_curl = (new CUrl())->setArgument('action', 'availreport.view');
-
-		$rows_per_page = (int) CWebUser::$data['rows_per_page'];
-        $selected_triggers = [];
-		$i = 0;  // Counter. We need to stop doing expensive calculateAvailability() when results are not visible
-		$page = $filter['page'] ? $filter['page'] - 1 : 0;
-		$start_idx = $page * $rows_per_page;
-		$end_idx = $start_idx + $rows_per_page;
-		foreach ($triggers as &$trigger) {
-			if ($generating_csv_flag ||
-				 ($i >= $start_idx && $i < $end_idx) ) {
-				$trigger['availability'] = calculateAvailability($trigger['triggerid'], $filter['from_ts'], $filter['to_ts']);
-				// if ($filter['only_with_problems']) {
-				if ($trigger['availability']['true'] > 0.00005) {
-					$selected_triggers[] = $trigger;
-				}
-				// } 
-				else {
-					$selected_triggers[] = $trigger;
-				}
-			} 
-			else {
-				$selected_triggers[] = $trigger;
-			}
-			$i++;
-		}
-
-		if (!$generating_csv_flag) {
-			// Not exporting data to CSV, just showing the data
-			// Split result array and create paging. Only if not generating CSV.
-			$paging = CPagerHelper::paginate($filter['page'], $selected_triggers, 'ASC', $view_curl);
-		}
-
-		foreach ($selected_triggers as &$trigger) {
-			$trigger['host_name'] = $trigger['hosts'][0]['name'];
-		}
-		unset($trigger);
-
-		return [
-			'paging' => $paging,
-			'triggers' => $selected_triggers,
-			'warning' => $warning
+	/**
+	 * Get additional data required for render filter as HTML.
+	 *
+	 * @param array $filter  Filter fields values.
+	 *
+	 * @return array
+	 */
+	protected function getAdditionalData(array $filter): array {
+		$data = [
+			'groups' => [],
+			'hosts' => [],
+			'triggers' => []
 		];
-	}
 
-	protected function cleanInput(array $input): array {
-		if (array_key_exists('filter_reset', $input) && $input['filter_reset']) {
-			return array_intersect_key(['filter_name' => ''], $input);
-		}
-		return $input;
-	}
-
-	protected function getAdditionalData($filter): array {
-		$data = [];
-
-		if ($filter['tpl_groupids']) {
-			$groups = API::HostGroup()->get([
+		// Host groups multiselect.
+		if ($filter['groupids']) {
+			$host_groups = API::HostGroup()->get([
 				'output' => ['groupid', 'name'],
-				'groupids' => $filter['tpl_groupids']
+				'groupids' => $filter['groupids'],
+				'preservekeys' => true
 			]);
-			$data['tpl_groups_multiselect'] = CArrayHelper::renameObjectsKeys(array_values($groups), ['groupid' => 'id']);
-		}
-
-		if ($filter['templateids']) {
-			$templates= API::Template()->get([
-				'output' => ['templateid', 'name'],
-				'templateids' => $filter['templateids']
-			]);
-			$data['templates_multiselect'] = CArrayHelper::renameObjectsKeys(array_values($templates), ['templateid' => 'id']);
-		}
-
-		if ($filter['tpl_triggerids']) {
-			$triggers = API::Trigger()->get([
-				'output' => ['triggerid', 'description'],
-				'selectHosts' => 'extend',
-				'triggerids' => $filter['tpl_triggerids']
-			]);
-
-			foreach($triggers as &$trigger) {
-				sizeof($trigger['hosts']) > 0 ?
-					$trigger['name'] = $trigger['hosts'][0]['host'] . ': ' . $trigger['description'] :
-					$trigger['name'] = $trigger['description'];
-				unset($trigger['hosts']);
-				unset($trigger['description']);
-			}
-
-			$data['tpl_triggers_multiselect'] = CArrayHelper::renameObjectsKeys(array_values($triggers), ['triggerid' => 'id']);
+			$data['groups'] = CArrayHelper::renameObjectsKeys($host_groups, ['groupid' => 'id']);
 		}
 
 		// Triggers multiselect.
@@ -249,48 +113,86 @@ abstract class CControllerBGAvailReport extends CController {
 			$data['triggers'] = $triggers;
 		}
 
-		if ($filter['hostgroupids']) {
-			$hostgroups = API::HostGroup()->get([
-				'output' => ['groupid', 'name'],
-				'groupids' => $filter['hostgroupids']
-			]);
-			$data['hostgroups_multiselect'] = CArrayHelper::renameObjectsKeys(array_values($hostgroups), ['groupid' => 'id']);
-		}
-
+		// Hosts multiselect.
 		if ($filter['hostids']) {
-			$hosts = API::Host()->get([
+			$data['hosts'] = CArrayHelper::renameObjectsKeys(API::Host()->get([
 				'output' => ['hostid', 'name'],
 				'hostids' => $filter['hostids']
-			]);
-			$data['hosts_multiselect'] = CArrayHelper::renameObjectsKeys(array_values($hosts), ['hostid' => 'id']);
+			]), ['hostid' => 'id']);
 		}
 
 		return $data;
 	}
 
-	protected function getChildGroups($parent_group_ids): array {
-		$all_group_ids = [];
-		foreach($parent_group_ids as $parent_group_id) {
-			$groups = API::HostGroup()->get([
-				'output' => ['groupid', 'name'],
-				'groupids' => [$parent_group_id]
-			]);
-			$parent_group_name = $groups[0]['name'].'/';
-			$len = strlen($parent_group_name);
+	/**
+	 * Clean and convert passed filter input fields from default values required for HTML presentation.
+	 *
+	 * @param array $input  Filter fields values.
+	 *
+	 * @return array
+	 */
+	protected function cleanInput(array $input): array {
+		if (array_key_exists('tags', $input) && $input['tags']) {
+			$input['tags'] = array_filter($input['tags'], function($tag) {
+				return !($tag['tag'] === '' && $tag['value'] === '');
+			});
+			$input['tags'] = array_values($input['tags']);
+		}
 
-			$groups = API::HostGroup()->get([
-				'output' => ['groupid', 'name'],
-				'search' => ['name' => $parent_group_name],
-				'startSearch' => true
-			]);
+		if (array_key_exists('inventory', $input) && $input['inventory']) {
+			$input['inventory'] = array_filter($input['inventory'], function($inventory) {
+				return $inventory['value'] !== '';
+			});
+			$input['inventory'] = array_values($input['inventory']);
+		}
 
-			$all_group_ids[] = $parent_group_id;
-			foreach ($groups as $group) {
-				if (substr($group['name'], 0, $len) === $parent_group_name) {
-					$all_group_ids[] = $group['groupid'];
-				}
+		return $input;
+	}
+
+	/**
+	 * Validate input of filter inventory fields.
+	 *
+	 * @return bool
+	 */
+	protected function validateInventory(): bool {
+		if (!$this->hasInput('inventory')) {
+			return true;
+		}
+
+		$ret = true;
+		foreach ($this->getInput('inventory') as $filter_inventory) {
+			if (count($filter_inventory) != 2
+					|| !array_key_exists('field', $filter_inventory) || !is_string($filter_inventory['field'])
+					|| !array_key_exists('value', $filter_inventory) || !is_string($filter_inventory['value'])) {
+				$ret = false;
+				break;
 			}
 		}
-		return $all_group_ids;
+
+		return $ret;
+	}
+
+	/**
+	 * Validate values of filter tags input fields.
+	 *
+	 * @return bool
+	 */
+	protected function validateTags(): bool {
+		if (!$this->hasInput('tags')) {
+			return true;
+		}
+
+		$ret = true;
+		foreach ($this->getInput('tags') as $filter_tag) {
+			if (count($filter_tag) != 3
+					|| !array_key_exists('tag', $filter_tag) || !is_string($filter_tag['tag'])
+					|| !array_key_exists('value', $filter_tag) || !is_string($filter_tag['value'])
+					|| !array_key_exists('operator', $filter_tag) || !is_string($filter_tag['operator'])) {
+				$ret = false;
+				break;
+			}
+		}
+
+		return $ret;
 	}
 }
--- actions/CControllerBGAvailReportView.php
+++ actions/CControllerBGAvailReportView.php
@@ -11,35 +11,51 @@ use CWebUser;
 
 class CControllerBGAvailReportView extends CControllerBGAvailReport {
 
-	protected function init() {
+	protected function init(): void {
 		$this->disableCsrfValidation();
 	}
 
-	protected function checkInput() {
+	protected function checkInput(): bool {
 		$fields = [
-			'name' =>			'string',
-			'mode' =>			'in '.AVAILABILITY_REPORT_BY_HOST.','.AVAILABILITY_REPORT_BY_TEMPLATE,
-			'tpl_groupids' =>		'array_id',
-			'templateids' =>		'array_id',
-			'tpl_triggerids' =>		'array_id',
-			'triggerids' =>			'array_id',
-			'hostgroupids' =>		'array_id',
-			'hostids' =>			'array_id',
-			'filter_reset' =>		'in 1',
-			'only_with_problems' =>		'in 0,1',
-			'page' =>			'ge 1',
-			'counter_index' =>		'ge 0',
+			'show' =>					'in '.TRIGGERS_OPTION_RECENT_PROBLEM.','.TRIGGERS_OPTION_IN_PROBLEM.','.TRIGGERS_OPTION_ALL,
+			'groupids' =>				'array_id',
+			'hostids' =>				'array_id',
+			'triggerids' =>				'array_id',
+			'name' =>					'string',
+			'severities' =>				'array',
+			'age_state' =>				'in 0,1',
+			'age' =>					'int32',
+			'inventory' =>				'array',
+			'evaltype' =>				'in '.TAG_EVAL_TYPE_AND_OR.','.TAG_EVAL_TYPE_OR,
+			'tags' =>					'array',
+			'show_tags' =>				'in '.SHOW_TAGS_NONE.','.SHOW_TAGS_1.','.SHOW_TAGS_2.','.SHOW_TAGS_3,
+			'show_symptoms' =>			'in 0,1',
+			'show_suppressed' =>		'in 0,1',
+			'unacknowledged' =>			'in 0,1',
+			'compact_view' =>			'in 0,1',
+			'show_timeline' =>			'in '.ZBX_TIMELINE_OFF.','.ZBX_TIMELINE_ON,
+			'details' =>				'in 0,1',
+			'highlight_row' =>			'in 0,1',
+			'show_opdata' =>			'in '.OPERATIONAL_DATA_SHOW_NONE.','.OPERATIONAL_DATA_SHOW_SEPARATELY.','.OPERATIONAL_DATA_SHOW_WITH_PROBLEM,
+			'tag_name_format' =>		'in '.TAG_NAME_FULL.','.TAG_NAME_SHORTENED.','.TAG_NAME_NONE,
+			'tag_priority' =>			'string',
+			'from' =>					'range_time',
+			'to' =>						'range_time',
 			'sort' =>					'in clock,host,severity,name',
-			'sortorder' =>				'in '.ZBX_SORT_DOWN.','.ZBX_SORT_UP,	
-			'from' =>			'range_time',
-			'to' =>				'range_time',
+			'sortorder' =>				'in '.ZBX_SORT_DOWN.','.ZBX_SORT_UP,
+			'page' =>					'ge 1',
+			'uncheck' =>				'in 1',
 			'filter_name' =>			'string',
 			'filter_custom_time' =>		'in 1,0',
 			'filter_show_counter' =>	'in 1,0',
 			'filter_counters' =>		'in 1',
-			'filter_set' =>				'in 1'
+			'filter_set' =>				'in 1',
+			'filter_reset' =>			'in 1',
+			'counter_index' =>			'ge 0'
 		];
-		$ret = $this->validateInput($fields) && $this->validateTimeSelectorPeriod();
+
+		$ret = $this->validateInput($fields) && $this->validateTimeSelectorPeriod() && $this->validateInventory()
+			&& $this->validateTags();
 
 		if (!$ret) {
 			$this->setResponse(new CControllerResponseFatal());
@@ -48,33 +64,31 @@ class CControllerBGAvailReportView extends CControllerBGAvailReport {
 		return $ret;
 	}
 
-	protected function checkPermissions() {
-		return $this->checkAccess(CRoleHelper::UI_REPORTS_AVAILABILITY_REPORT);
+	protected function checkPermissions(): bool {
+		return $this->checkAccess(CRoleHelper::UI_MONITORING_PROBLEMS);
 	}
 
-	protected function doAction() {
-
+	protected function doAction(): void {
 		$filter_tabs = [];
+		$profile = (new CTabFilterProfile(static::FILTER_IDX, static::FILTER_FIELDS_DEFAULT))->read();
 
-		// $profile = (new CTabFilterProfile(static::FILTER_IDX, static::FILTER_FIELDS_DEFAULT))->read();
-		// if ($this->hasInput('filter_reset')) {
-		// 	$profile->reset();
-		// }
-
-		$filter_tabs = [];
-		$profile = (new CTabFilterProfile(static::FILTER_IDX, static::FILTER_FIELDS_DEFAULT))
-			->read()
-			->setInput($this->cleanInput($this->getInputAll()));
-			
-		// elseif ($this->hasInput('filter_set')) {
-		// 	$profile->setTabFilter(0, ['filter_name' => ''] + $this->cleanInput($this->getInputAll()));
-		// 	$profile->update();
-		// }
-		// else {
-		// 	$profile->setInput($this->cleanInput($this->getInputAll()));
-		// }
+		if ($this->hasInput('filter_reset')) {
+			$profile->reset();
+		}
+		elseif ($this->hasInput('filter_set')) {
+			$profile->setTabFilter(0, ['filter_name' => ''] + $this->cleanInput($this->getInputAll()));
+			$profile->update();
+		}
+		else {
+			$profile->setInput($this->cleanInput($this->getInputAll()));
+		}
 
 		foreach ($profile->getTabsWithDefaults() as $index => $filter_tab) {
+			if ($filter_tab['filter_custom_time']) {
+				$filter_tab['show'] = TRIGGERS_OPTION_ALL;
+				$filter_tab['filter_src']['show'] = TRIGGERS_OPTION_ALL;
+			}
+
 			if ($index == $profile->selected) {
 				// Initialize multiselect data for filter_scr to allow tabfilter correctly handle unsaved state.
 				$filter_tab['filter_src']['filter_view_data'] = $this->getAdditionalData($filter_tab['filter_src']);
@@ -83,20 +97,20 @@ class CControllerBGAvailReportView extends CControllerBGAvailReport {
 			$filter_tabs[] = $filter_tab + ['filter_view_data' => $this->getAdditionalData($filter_tab)];
 		}
 
-		// filter
 		$filter = $filter_tabs[$profile->selected];
-		$refresh_curl = (new CUrl('zabbix.php'));
-		$filter['action'] = 'availreport.view.refresh';
-		$filter['action_from_url'] = $this->getAction();
+		$refresh_curl = new CUrl('zabbix.php');
+		$filter['action'] = 'problem.view.refresh';
 		array_map([$refresh_curl, 'setArgument'], array_keys($filter), $filter);
-		// $timeselector_from = $filter['filter_custom_time'] == 0 ? $filter['from'] : $profile->from;
-		// $timeselector_to = $filter['filter_custom_time'] == 0 ? $filter['to'] : $profile->to;
-		
+
+		if (!$this->hasInput('page')) {
+			$refresh_curl->removeArgument('page');
+		}
+
 		$data = [
 			'action' => $this->getAction(),
 			'tabfilter_idx' => static::FILTER_IDX,
 			'filter' => $filter,
-			'filter_view' => 'reports.availreport.filter',
+			'filter_view' => 'monitoring.problem.filter',
 			'filter_defaults' => $profile->filter_defaults,
 			'tabfilter_options' => [
 				'idx' => static::FILTER_IDX,
@@ -104,23 +118,28 @@ class CControllerBGAvailReportView extends CControllerBGAvailReport {
 				'support_custom_time' => 1,
 				'expanded' => $profile->expanded,
 				'page' => $filter['page'],
-				// 'timeselector' => [
-				// 	'from' => $profile->from,
-				// 	'to' => $profile->to,
-				// 	'disabled' => true
-				// ] + getTimeselectorActions($profile->from, $profile->to)
+				'csrf_token' => CCsrfTokenHelper::get('tabfilter'),
+				'timeselector' => [
+					'from' => $profile->from,
+					'to' => $profile->to,
+					'disabled' => ($filter['show'] != TRIGGERS_OPTION_ALL || $filter['filter_custom_time'])
+				] + getTimeselectorActions($profile->from, $profile->to)
 			],
 			'filter_tabs' => $filter_tabs,
 			'refresh_url' => $refresh_curl->getUrl(),
-			'refresh_interval' => 3600000, //+++1000,
+			'refresh_interval' => CWebUser::getRefresh() * 1000,
+			'inventories' => array_column(getHostInventories(), 'title', 'db_field'),
+			'sort' => $filter['sort'],
+			'sortorder' => $filter['sortorder'],
+			'uncheck' => $this->hasInput('filter_reset'),
 			'page' => $this->getInput('page', 1)
-		] + $this->getData($filter);
+		];
 
 		$response = new CControllerResponseData($data);
-		$response->setTitle(_('Availability report'));
+		$response->setTitle(_('Problems'));
 
-		if ($data['action'] === 'availreport.view.csv') {
-			$response->setFileName('zbx_availability_report_export.csv');
+		if ($data['action'] === 'problem.view.csv') {
+			$response->setFileName('zbx_problems_export.csv');
 		}
 
 		$this->setResponse($response);
