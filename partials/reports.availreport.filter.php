fd24855959f58311798889e40105243dd2cd196a
lkaprielian <lisakaprielian.lk@gmail.com>
2024-04-08 19:13:58 +0200

    Update reports.availreport.filter.php

--- partials/reports.availreport.filter.php
+++ partials/reports.availreport.filter.php
@@ -376,7 +376,110 @@ if (array_key_exists('render_html', $data)) {
 		$('[name="filter_new"],[name="filter_update"]').hide()
 			.filter(data.filter_configurable ? '[name="filter_update"]' : '[name="filter_new"]').show();
 
+		let fields = ['show', 'name', 'tag_priority', 'show_opdata', 'show_symptoms', 'show_suppressed', 'show_tags',
+				'unacknowledged', 'compact_view', 'show_timeline', 'details', 'highlight_row', 'age_state', 'age',
+				'tag_name_format', 'evaltype'
+			],
+			eventHandler = {
+				show: () => {
+					// Dynamically disable hidden input elements to allow correct detection of unsaved changes.
+					var	filter_show = ($('input[name="show"]:checked', container).val() != <?= TRIGGERS_OPTION_ALL ?>),
+						filter_custom_time = $('[name="filter_custom_time"]', container).val(),
+						disabled = (!filter_show || !$('[name="age_state"]:checked', container).length);
 
+					$('[name="age"]', container).attr('disabled', disabled).closest('li').toggle(filter_show);
+					$('[name="age_state"]', container).attr('disabled', !filter_show);
+
+					if (!filter_show && filter_custom_time == 1) {
+						$('[name="show"]', container)
+							.prop('checked', false)
+							.attr('disabled', 'disabled')
+							.filter('[value="<?= TRIGGERS_OPTION_ALL ?>"]')
+							.prop('checked', true)
+							.removeAttr('disabled');
+						this.setUrlArgument('show', <?= TRIGGERS_OPTION_ALL ?>);
+					}
+					else {
+						$('[name="show"]', container).removeAttr('disabled');
+					}
+
+					if (this._parent) {
+						this._parent.updateTimeselector(this, filter_show || (filter_custom_time == 1));
+					}
+				},
+				age_state: (ev) => {
+					$('[name="age"]', container).prop('disabled', !$('[name="age_state"]', container).is(':checked'));
+				},
+				compact_view: () => {
+					let checked = $('[name="compact_view"]', container).is(':checked');
+
+					$('[name="show_timeline"]', container).prop('disabled', checked);
+					$('[name="details"]', container).prop('disabled', checked);
+					$('[name="show_opdata"]', container).prop('disabled', checked);
+					$('[name="highlight_row"]', container).prop('disabled', !checked);
+				},
+				show_tags: () => {
+					let disabled = ($('[name="show_tags"]:checked', container).val() == <?= SHOW_TAGS_NONE ?>);
+
+					$('[name="tag_priority"]', container).prop('disabled', disabled);
+					$('[name="tag_name_format"]', container).prop('disabled', disabled);
+				}
+			};
+
+		// Input, radio and single checkboxes.
+		fields.forEach((key) => {
+			var elm = $('[name="' + key + '"]', container);
+
+			if (elm.is(':radio,:checkbox')) {
+				elm.filter('[value="' + data[key] + '"]').attr('checked', true);
+			}
+			else {
+				elm.val(data[key]);
+			}
+		});
+
+		// Show timeline default value is checked and it will be rendered in template therefore initialize if unchecked.
+		$('[name="show_timeline"][unchecked-value="' + data['show_timeline'] + '"]', container).removeAttr('checked');
+
+		// Severities checkboxes.
+		for (const value in data.severities) {
+			$('[name="severities[' + value + ']"]', container).attr('checked', true);
+		}
+
+		// Inventory table.
+		if (data.inventory.length == 0) {
+			data.inventory.push({'field': '', 'value': ''});
+		}
+		$('#filter-inventory_' + data.uniqid, container).dynamicRows({
+			template: '#filter-inventory-row',
+			rows: data.inventory,
+			counter: 0
+		});
+
+		// Tags table.
+		if (data.tags.length == 0) {
+			data.tags.push({'tag': '', 'value': '', 'operator': <?= TAG_OPERATOR_LIKE ?>});
+		}
+
+		$('#filter-tags_' + data.uniqid, container)
+			.dynamicRows({
+				template: '#filter-tag-row-tmpl',
+				rows: data.tags,
+				counter: 0,
+				dataCallback: (tag) => {
+					tag.uniqid = data.uniqid
+					return tag;
+				}
+			})
+			.on('afteradd.dynamicRows', function() {
+				var rows = this.querySelectorAll('.form_row');
+				new CTagFilterItem(rows[rows.length - 1]);
+			});
+
+		// Init existing fields once loaded.
+		document.querySelectorAll('#filter-tags_' + data.uniqid + ' .form_row').forEach(row => {
+			new CTagFilterItem(row);
+		});
 
 		// Host groups multiselect.
 		$('#groupids_' + data.uniqid, container).multiSelectHelper({
@@ -445,23 +548,62 @@ if (array_key_exists('render_html', $data)) {
 			}
 		});
 
-		// $('#show_' + data.uniqid, container).change(eventHandler.show).trigger('change');
-		// $('[name="age_state"]').change(eventHandler.age_state).trigger('change');
-		// $('[name="compact_view"]', container).change(eventHandler.compact_view).trigger('change');
-		// $('[name="show_tags"]', container).change(eventHandler.show_tags).trigger('change');
+		$('#show_' + data.uniqid, container).change(eventHandler.show).trigger('change');
+		$('[name="age_state"]').change(eventHandler.age_state).trigger('change');
+		$('[name="compact_view"]', container).change(eventHandler.compact_view).trigger('change');
+		$('[name="show_tags"]', container).change(eventHandler.show_tags).trigger('change');
 
 		// Initialize src_url.
 		this.resetUnsavedState();
 		this.on(TABFILTERITEM_EVENT_ACTION, update.bind(this));
 
+		if (this._parent) {
+			this._parent.on(TABFILTER_EVENT_UPDATE, (ev) => {
+				let form = this.getForm(),
+					tabfilter = ev.detail.target;
+
+				if (ev.detail.filter_property !== 'properties' || tabfilter._active_item !== this) {
+					return;
+				}
+
+				if ($(form).find('[name="filter_custom_time"]').val() == 1) {
+					$('[name="show"][value="<?= TRIGGERS_OPTION_ALL ?>"]', form).prop('checked', true);
+					$('#show_' + this._data.uniqid, form).trigger('change');
+					this.setUrlArgument('show', <?= TRIGGERS_OPTION_ALL ?>);
+					this.updateUnsavedState();
+					this.setBrowserLocationToApplyUrl();
+				}
+			});
+
+			this._parent.on(TABFILTER_EVENT_NEWITEM, () => {
+				let form = this.getForm();
+
+				if ($(form).find('[name="filter_custom_time"]').val() == 1) {
+					$('[name="show"][value="<?= TRIGGERS_OPTION_ALL ?>"]', form).prop('checked', true);
+				}
+			});
+		}
 	}
 
 	function expand(data, container) {
 		// "Save as" can contain only home tab, also home tab cannot contain "Update" button.
 		$('[name="filter_new"],[name="filter_update"]').hide()
 			.filter(data.filter_configurable ? '[name="filter_update"]' : '[name="filter_new"]').show();
+
+		// Trigger change to update timeselector ui disabled state.
+		$('#show_' + data.uniqid, container).trigger('change');
 	}
 
+	function select(data, container) {
+		if (this._template_rendered) {
+			// Template rendered, use element events, trigger change to update timeselector ui disabled state.
+			$('#show_' + data.uniqid, container).trigger('change');
+		}
+		else if (this._parent) {
+			// Template is not rendered, use input data.
+			this._parent.updateTimeselector(this, (data.show != <?= TRIGGERS_OPTION_ALL ?>));
+		}
+	}
 
 	/**
 	 * On filter apply or update buttons press update disabled UI fields.
@@ -476,6 +618,18 @@ if (array_key_exists('render_html', $data)) {
 			return;
 		}
 
+		$('[name="highlight_row"],[name="details"],[name="show_timeline"]', container)
+			.filter(':disabled')
+			.prop('checked', false);
+
+		$('[name="show_opdata"]:disabled', container)
+			.prop('checked', false)
+			.filter('[value="' + <?= CControllerProblem::FILTER_FIELDS_DEFAULT['show_opdata'] ?> +'"]')
+			.prop('checked', true);
+
+		if ($('[name="age_state"]', container).not(':checked').length) {
+			$('[name="age"]').val(<?= CControllerProblem::FILTER_FIELDS_DEFAULT['age'] ?>);
+		}
 	}
 
 	// Tab filter item events handlers.
