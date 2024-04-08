adaaf1c472239ed49d1aa3deb60e66ae06d4656e
lkaprielian <lisakaprielian.lk@gmail.com>
2024-04-08 16:47:54 +0200

    update

--- partials/reports.availreport.view.html.php
+++ partials/reports.availreport.view.html.php
@@ -1,77 +1,106 @@
-<?php declare(strict_types = 0);
-/*
-** Zabbix
-** Copyright (C) 2001-2024 Zabbix SIA
-**
-** This program is free software; you can redistribute it and/or modify
-** it under the terms of the GNU General Public License as published by
-** the Free Software Foundation; either version 2 of the License, or
-** (at your option) any later version.
-**
-** This program is distributed in the hope that it will be useful,
-** but WITHOUT ANY WARRANTY; without even the implied warranty of
-** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-** GNU General Public License for more details.
-**
-** You should have received a copy of the GNU General Public License
-** along with this program; if not, write to the Free Software
-** Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
-**/
+<?php
+
+$form = (new CForm())->setName('availreport_view');
+
+$table = (new CTableInfo());
+
+$view_url = $data['view_curl']->getUrl();
 
 
-/**
- * @var CPartial $this
- * @var array    $data
- */
 
-$options = [
-	'resourcetype' => SCREEN_RESOURCE_PROBLEM,
-	'mode' => SCREEN_MODE_JS,
-	'dataId' => 'problem',
-	'page' => $data['page'],
-	'data' => [
-		'action' => $data['action'],
-		'sort' => $data['sort'],
-		'sortorder' => $data['sortorder'],
-		'filter' => [
-			'show' => $data['filter']['show'],
-			'groupids' => $data['filter']['groupids'],
-			'hostids' => $data['filter']['hostids'],
-			'triggerids' => $data['filter']['triggerids'],
-			'name' => $data['filter']['name'],
-			'severities' => $data['filter']['severities'],
-			'inventory' => $data['filter']['inventory'],
-			'evaltype' => $data['filter']['evaltype'],
-			'tags' => $data['filter']['tags'],
-			'show_tags' => $data['filter']['show_tags'],
-			'tag_name_format' => $data['filter']['tag_name_format'],
-			'tag_priority' => $data['filter']['tag_priority'],
-			'show_symptoms' => $data['filter']['show_symptoms'],
-			'show_suppressed' => $data['filter']['show_suppressed'],
-			'unacknowledged' => $data['filter']['unacknowledged'],
-			'compact_view' => $data['filter']['compact_view'],
-			'show_timeline' => $data['filter']['show_timeline'],
-			'details' => $data['filter']['details'],
-			'highlight_row' => $data['filter']['highlight_row'],
-			'show_opdata' => $data['filter']['show_opdata']
-		],
-		'limit' => CSettingsHelper::get(CSettingsHelper::SEARCH_LIMIT)
-	]
-];
+// $options = [
+// 	'mode' => SCREEN_MODE_JS,
+// 	'page' => $data['paging'],
+// 	'data' => [
+// 		'action' => $data['action'],
+// 		'sort' => $data['sort'],
+// 		'sortorder' => $data['sortorder'],
+// 		'filter' => [
+// 			'groupids' => $data['filter']['groupids'],
+// 			'hostids' => $data['filter']['hostids'],
+// 			'triggerids' => $data['filter']['triggerids']
+// 		],
+// 		'limit' => CSettingsHelper::get(CSettingsHelper::SEARCH_LIMIT)
+// 	]
+// ];
 
-switch ($data['filter']['show']) {
-	case TRIGGERS_OPTION_RECENT_PROBLEM:
-	case TRIGGERS_OPTION_IN_PROBLEM:
-		$options['data']['filter']['age_state'] = $data['filter']['age_state'];
-		$options['data']['filter']['age'] = $data['filter']['age'];
-		break;
+// echo CScreenBuilder::getScreen($options)->get();
 
-	case TRIGGERS_OPTION_ALL:
-		$options['profileIdx'] = $data['tabfilter_idx'];
-		$options['profileIdx2'] = 0;
-		$options['from'] = $data['filter']['from'];
-		$options['to'] = $data['filter']['to'];
-		break;
+// print_r($data['filter']['sort']);
+
+// if (array_key_exists('view_curl', $data)) {
+// 	$view_url = $data['view_curl'];
+// 	$arr = explode('&', $view_url, -1);
+// 	print_r($arr);
+// }
+// $filter = $data['filter'];
+
+$table->setHeader([
+	(new CColHeader(_('Host'))),
+	(new CColHeader(_('Trigger'))),
+	(new CColHeader(_('Problems'))),
+	(new CColHeader(_('Ok'))),
+	// (new CColHeader(_('Tags'))),
+	make_sorting_header(_('Number of triggers'), 'cnt_event', $data['filter']['sort'], $data['filter']['sortorder'], $view_url)
+]);
+
+$allowed_ui_problems = CWebUser::checkAccess(CRoleHelper::UI_MONITORING_PROBLEMS);
+
+$triggers = $data['triggers'];
+
+// $tags = makeTags($triggers, true, 'triggerid', ZBX_TAG_COUNT_DEFAULT);
+// foreach ($triggers as &$trigger) {
+// 	$trigger['tags'] = $tags[$trigger['triggerid']];
+// }
+// unset($trigger);
+
+foreach ($triggers as $trigger) {
+
+	$hostId = $trigger['hosts'][0]['hostid'];
+	
+	$hostName = (new CLinkAction($trigger['hosts'][0]['name']))->setMenuPopup(CMenuPopupHelper::getHost($hostId));
+	// if ($data['hosts'][0]['status'] == HOST_STATUS_NOT_MONITORED) {
+	// 	$hostName->addClass(ZBX_STYLE_RED);
+	// }
+
+	$triggerDescription = (new CLinkAction($trigger['description']))
+		->setMenuPopup(CMenuPopupHelper::getTrigger([
+			'triggerid' => $trigger['triggerid'],
+			'backurl' => (new CUrl('toptriggers.php'))->getUrl()
+	]));
+
+
+	$table->addRow([
+		$hostName,
+		// $allowed_ui_problems
+		// 	? new CLink($trigger['description'],
+		// 		(new CUrl('zabbix.php'))
+		// 			->setArgument('action', 'problem.view')
+		// 			->setArgument('filter_name', '')
+		// 			->setArgument('triggerids', [$trigger['triggerid']])
+		// 	)
+		// 	: $trigger['description'],
+		$allowed_ui_problems ? $triggerDescription : $trigger['description'],
+		// $triggerDescription,
+		($trigger['availability']['true'] < 0.00005)
+			? ''
+			: (new CSpan(sprintf('%.4f%%', $trigger['availability']['true'])))->addClass(ZBX_STYLE_RED),
+		($trigger['availability']['false'] < 0.00005)
+			? ''
+			: (new CSpan(sprintf('%.4f%%', $trigger['availability']['false'])))->addClass(ZBX_STYLE_GREEN),
+		// $trigger['tags'],
+		$trigger['cnt_event']
+	]);
 }
 
-echo CScreenBuilder::getScreen($options)->get();
+// $data = [
+// 	'view_curl' => $view_url,
+// 	'filter' => $filter,
+// 	'paging' => $data['paging']
+// ];
+
+// print_r($data)
+$form->addItem([$table,	$data['paging']]);
+
+echo $form;
+?>
