f19b4117dda05f161879f933e62566c6d3033748
lkaprielian <lisakaprielian.lk@gmail.com>
2024-04-08 18:22:46 +0200

    update

--- actions/CControllerBGAvailReport.php
+++ actions/CControllerBGAvailReport.php
@@ -14,41 +14,29 @@ use CWebUser;
 abstract class CControllerBGAvailReport extends CController {
 
 	// Filter idx prefix.
-	// const FILTER_IDX = 'web.avail_report.filter';
+	const FILTER_IDX = 'web.avail_report.filter';
 	// const FILTER_IDX = 'web.toptriggers.filter';
 	// const FILTER_IDX = 'web.monitoring.host';
-	const FILTER_IDX = 'web.monitoring.problem';
+	// const FILTER_IDX = 'web.monitoring.problem';
 
 
 
+	// Filter fields default values.
 	const FILTER_FIELDS_DEFAULT = [
-		'show' => TRIGGERS_OPTION_RECENT_PROBLEM,
-		'groupids' => [],
-		'hostids' => [],
-		'triggerids' => [],
 		'name' => '',
-		'severities' => [],
-		'age_state' => 0,
-		'age' => 14,
-		'inventory' => [],
-		'evaltype' => TAG_EVAL_TYPE_AND_OR,
-		'tags' => [],
-		'show_tags' => SHOW_TAGS_3,
-		'show_symptoms' => 0,
-		'show_suppressed' => 0,
-		'unacknowledged' => 0,
-		'compact_view' => 0,
-		'show_timeline' => ZBX_TIMELINE_ON,
-		'details' => 0,
-		'highlight_row' => 0,
-		'show_opdata' => OPERATIONAL_DATA_SHOW_NONE,
-		'tag_name_format' => TAG_NAME_FULL,
-		'tag_priority' => '',
-		'page' => null,
-		'sort' => 'clock',
-		'sortorder' => ZBX_SORT_DOWN,
+		'mode' => AVAILABILITY_REPORT_BY_TEMPLATE,
+		'tpl_groupids' => [],
+		'templateids' => [],
+		'tpl_triggerids' => [],
+		'triggerids' => [],
+		'hostgroupids' => [],
+		'hostids' => [],
+		'only_with_problems' => 1,
+        'page' => null,
 		'from' => '',
-		'to' => ''
+		'to' => '',
+		'sort' => 'name',
+		'sortorder' => ZBX_SORT_DOWN
 	];
 
 	protected function getData(array $filter): array {
